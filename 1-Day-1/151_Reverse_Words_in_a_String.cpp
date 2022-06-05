e4f8dd39f6e34e4b846f8d15c6283b07a37594c5
sayan112 <sayanmaitraj5@gmail.com>
2022-06-05 17:08:57 +0530

    Brute Force solution Upload

---
+++ 1-Day-1/151_Reverse_Words_in_a_String.cpp
@@ -0,0 +1,55 @@
+// Brute Force Solution
+
+151. Reverse Words in a String (Leetcode Medium )
+
+    class Solution
+{
+public:
+    string reverseWords(string str)
+    {
+        string need = "";
+        vector<string> str2;
+        for (int i = 0; i < str.size(); i++)
+
+        {
+
+            if (str[i] == ' ')
+            {
+                if ((need.size() >= 1))
+                {
+                    if (need[0] != ' ')
+                    {
+                        str2.push_back(need);
+                    }
+                }
+                need = "";
+            }
+            else
+            {
+                need += str[i];
+            }
+            if (i == str.size() - 1)
+            {
+                if (need != "")
+                {
+                    str2.push_back(need);
+                }
+            }
+        }
+
+        string ans;
+        for (int i = str2.size() - 1; i >= 0; i--)
+        {
+            ans += str2[i];
+            if (i != 0)
+            {
+                ans += ' ';
+            }
+        }
+
+        return ans;
+    }
+};
+
+Find the whole doucment ~ https: // nutritious-closet-2ba.notion.site/151-Reverse-Words-in-a-String-b98b55ef07d04498bcbc5d6548c000b2
+
